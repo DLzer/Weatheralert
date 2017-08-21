d6ae0136e511f054e3ea019f6933e45de2d1786e
Dillon Lomnitzer <yodaesu@gmail.com>
2017-08-21 15:35:55 -0400

    added hash and form validation

--- footer.php
+++ footer.php
@@ -1,5 +1,7 @@
         <script src="js/vendor.js"></script>
         <script src="js/app.js"></script>
+        <script src="js/forms.js"></script>
+        <script src="js/sha512.js"></script>
     </body>
 
 </html>
