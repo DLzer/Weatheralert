de670e3a8d593a46e1771271ea5109e234d21609
Dillon Lomnitzer <yodaesu@gmail.com>
2017-08-21 15:36:41 -0400

    New user signup

---
+++ register.php
@@ -0,0 +1,92 @@
+<?php
+include_once 'includes/register.inc.php';
+include_once 'includes/functions.php';
+?>
+
+<!doctype html>
+<html class="no-js" lang="en">
+
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="x-ua-compatible" content="ie=edge">
+        <title> ModularAdmin - Free Dashboard Theme | HTML Version </title>
+        <meta name="description" content="">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <link rel="apple-touch-icon" href="apple-touch-icon.png">
+        <!-- Place favicon.ico in the root directory -->
+        <link rel="stylesheet" href="css/vendor.css">
+        <!-- Theme initialization -->
+        <script>
+            var themeSettings = (localStorage.getItem('themeSettings')) ? JSON.parse(localStorage.getItem('themeSettings')) :
+            {};
+            var themeName = themeSettings.themeName || '';
+            if (themeName)
+            {
+                document.write('<link rel="stylesheet" id="theme-style" href="css/app-' + themeName + '.css">');
+            }
+            else
+            {
+                document.write('<link rel="stylesheet" id="theme-style" href="css/app.css">');
+            }
+        </script>
+    </head>
+
+    <body>
+        <div class="auth">
+            <div class="auth-container">
+                <div class="card">
+                    <header class="auth-header">
+                        <h1 class="auth-title">
+                            <div class="logo"> <span class="l l1"></span> <span class="l l2"></span> <span class="l l3"></span> <span class="l l4"></span> <span class="l l5"></span> </div> ModularAdmin </h1>
+                    </header>
+                    <div class="auth-content">
+                        <p class="text-xs-center">SIGNUP TO GET INSTANT ACCESS</p>
+                        <?php
+                            if (!empty($error_msg)) {
+                            echo $error_msg;
+                        }
+                        ?>
+                        <form id="signup-form" action="<?php echo esc_url($_SERVER['REQUEST_URI']); ?>" method="GET" novalidate="">
+                            <div class="form-group"> <label for="firstname">Username</label>
+                                <div class="row">
+                                    <div class="col-sm-6"> <input type="text" class="form-control underlined" name="username" id="firstname" placeholder="Enter Username" required=""> </div>
+                                </div>
+                            </div>
+                            <div class="form-group"> <label for="email">Email</label> <input type="email" class="form-control underlined" name="email" id="email" placeholder="Enter email address" required=""> </div>
+                            <div class="form-group"> <label for="password">Password</label>
+                                <div class="row">
+                                    <div class="col-sm-6"> <input type="password" class="form-control underlined" name="password" id="password" placeholder="Enter password" required=""> </div>
+                                    <div class="col-sm-6"> <input type="password" class="form-control underlined" name="retype_password" id="retype_password" placeholder="Re-type password" required=""> </div>
+                                </div>
+                            </div>
+                            <div class="form-group"> <label for="agree">
+            <input class="checkbox" name="agree" id="agree" type="checkbox" required=""> 
+            <span>Agree the terms and <a href="#">policy</a></span>
+          </label> </div>
+                            <div class="form-group"> <button type="submit" class="btn btn-block btn-primary" onclick="return regformhash(this.form,
+                                   this.form.username,
+                                   this.form.email,
+                                   this.form.password,
+                                   this.form.confirmpwd);">Sign Up</button> </div>
+                            <div class="form-group">
+                                <p class="text-muted text-xs-center">Already have an account? <a href="index.php">Login!</a></p>
+                            </div>
+                        </form>
+                    </div>
+                </div>
+                <div class="text-xs-center"> <a href="index.php" class="btn btn-secondary rounded btn-sm">
+      <i class="fa fa-arrow-left"></i> Back to dashboard
+    </a> </div>
+            </div>
+        </div>
+        <!-- Reference block for JS -->
+        <div class="ref" id="ref">
+            <div class="color-primary"></div>
+            <div class="chart">
+                <div class="color-primary"></div>
+                <div class="color-secondary"></div>
+            </div>
+        </div>
+       
+
+<?php include('footer.php'); ?>
