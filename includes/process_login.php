59a4eaa568d3afe41033822f8b7a80dedc94857d
Dillon Lomnitzer <yodaesu@gmail.com>
2017-08-22 09:43:22 -0400

    process login

--- includes/process_login.php
+++ includes/process_login.php
@@ -1,22 +1,17 @@
 <?php
 
 include_once 'connect.php';
-include_once 'functions.php';
- 
-sec_session_start(); // Our custom secure way of starting a PHP session.
 
-if (isset($_POST['email'], $_POST['password'])) {
-   $email = $_POST['email'];
-   $password = $_POST['password']; // The hashed password.
-
-   if (my_login($email, $password, $mysqli) == true) {
-       // Login success 
-       header('Location: dashboard.php');
-   } else {
-       // Login failed 
-       header('Location: ../index.php?error=1');
-   }
-} else {
-   // The correct POST variables were not sent to this page. 
-   echo 'Invalid Request';
+function getUsernamePass($db) {
+$stmt = $db->prepare("SELECT email, password FROM wx_users WHERE email=? ");
+$stmt->execute(array($email, $password));
+$rows = $stmt->fetchAll(PDO::FETCH_ASSOC);
 }
+
+try {
+    getUsernamePass($db);
+} catch(PDOException $ex) {
+    echo "An Error occured!";
+}
+
+?>
