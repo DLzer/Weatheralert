060d9e2c88ad902f08d461e38952b7460bedaf0a
Dillon Lomnitzer <yodaesu@gmail.com>
2017-08-22 09:41:04 -0400

    added alert history table

---
+++ alerthistory.php
@@ -0,0 +1,122 @@
+<?php include('header.php'); ?>
+
+                <article class="content responsive-tables-page">
+                    <section class="section">
+                        <div class="row">
+                            <div class="col-md-12">
+                                <div class="card">
+                                    <div class="card-block">
+                                        <div class="card-title-block">
+                                            <h3 class="title"> WX-Alert Users</h3>
+                                        </div>
+                                        <section class="example">
+                                            <div class="table-flip-scroll">
+                                                <table class="table table-striped table-bordered table-hover flip-content">
+                                                    <thead class="flip-header">
+                                                        <tr>
+                                                            <th>Name</th>
+                                                            <th>Business</th>
+                                                            <th>Phone</th>
+                                                            <th>Date</th>
+                                                            <th>Edit</th>
+                                                        </tr>
+                                                    </thead>
+                                                    <tbody>
+                                                        <tr class="odd gradeX">
+                                                            <td>Trident</td>
+                                                            <td>Internet Explorer 4.0</td>
+                                                            <td>Win 95+</td>
+                                                            <td class="center">4</td>
+                                                            <td class="center">X</td>
+                                                        </tr>
+                                                        <tr class="even gradeC">
+                                                            <td>Trident</td>
+                                                            <td>Internet Explorer 5.0</td>
+                                                            <td>Win 95+</td>
+                                                            <td class="center">5</td>
+                                                            <td class="center">C</td>
+                                                        </tr>
+                                                        <tr class="odd gradeA">
+                                                            <td>Trident</td>
+                                                            <td>Internet Explorer 5.5</td>
+                                                            <td>Win 95+</td>
+                                                            <td class="center">5.5</td>
+                                                            <td class="center">A</td>
+                                                        </tr>
+                                                        <tr class="even gradeA">
+                                                            <td>Trident</td>
+                                                            <td>Internet Explorer 6</td>
+                                                            <td>Win 98+</td>
+                                                            <td class="center">6</td>
+                                                            <td class="center">A</td>
+                                                        </tr>
+                                                        <tr class="odd gradeA">
+                                                            <td>Trident</td>
+                                                            <td>Internet Explorer 7</td>
+                                                            <td>Win XP SP2+</td>
+                                                            <td class="center">7</td>
+                                                            <td class="center">A</td>
+                                                        </tr>
+                                                        <tr class="even gradeA">
+                                                            <td>Trident</td>
+                                                            <td>AOL browser (AOL desktop)</td>
+                                                            <td>Win XP</td>
+                                                            <td class="center">6</td>
+                                                            <td class="center">A</td>
+                                                        </tr>
+                                                        <tr class="gradeA">
+                                                            <td>Gecko</td>
+                                                            <td>Firefox 1.0</td>
+                                                            <td>Win 98+ / OSX.2+</td>
+                                                            <td class="center">1.7</td>
+                                                            <td class="center">A</td>
+                                                        </tr>
+                                                        <tr class="gradeA">
+                                                            <td>Gecko</td>
+                                                            <td>Firefox 1.5</td>
+                                                            <td>Win 98+ / OSX.2+</td>
+                                                            <td class="center">1.8</td>
+                                                            <td class="center">A</td>
+                                                        </tr>
+                                                        <tr class="gradeA">
+                                                            <td>Gecko</td>
+                                                            <td>Firefox 2.0</td>
+                                                            <td>Win 98+ / OSX.2+</td>
+                                                            <td class="center">1.8</td>
+                                                            <td class="center">A</td>
+                                                        </tr>
+                                                        <tr class="gradeA">
+                                                            <td>Gecko</td>
+                                                            <td>Firefox 3.0</td>
+                                                            <td>Win 2k+ / OSX.3+</td>
+                                                            <td class="center">1.9</td>
+                                                            <td class="center">A</td>
+                                                        </tr>
+                                                        <tr class="gradeA">
+                                                            <td>Gecko</td>
+                                                            <td>Camino 1.0</td>
+                                                            <td>OSX.2+</td>
+                                                            <td class="center">1.8</td>
+                                                            <td class="center">A</td>
+                                                        </tr>
+                                                        <tr class="gradeA">
+                                                            <td>Gecko</td>
+                                                            <td>Camino 1.5</td>
+                                                            <td>OSX.3+</td>
+                                                            <td class="center">1.8</td>
+                                                            <td class="center">A</td>
+                                                        </tr>
+                                                    </tbody>
+                                                </table>
+                                            </div>
+                                        </section>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </section>
+                </article>
+
+
+
+<?php include('footer.php'); ?>
